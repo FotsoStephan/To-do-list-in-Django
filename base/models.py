0f2c98fd0bcdd0f99d2e90c18abf5d87c538e48f
Stephan_Joel <stephanjoel6985@gmail.com>
2022-12-02 02:39:21 +0100

    creating a model Task and making migrations

--- base/models.py
+++ base/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    #on_delete=models.CASCADE means if the user gets deleted, the task remains
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    # we want to automatically populate the time when a task is created
+    create = models.DateTimeField(auto_now_add=True)
+    
+    def __self__(self):
+        return self.title
+    
+    class  Meta:
+        ordering = ['complete']
